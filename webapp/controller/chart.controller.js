f7c4120bf7715aa0b849af2ece48cc117eb7d404
Gagliardi Diego <dgagliardi92@gmail.com>
2018-09-24 12:33:29 +0000

    V1.1

--- webapp/controller/chart.controller.js
+++ webapp/controller/chart.controller.js
@@ -11,53 +11,108 @@ sap.ui.define([
 			//this.mapingOData(this.getView().byId("idcolumn2"));
 			this.mapingOData(this, this.getView().byId("idcolumn"));
 			//this.maping(this.getView().byId("idcolumn"));
-
+	
+			this.getView().byId("cont3").setTitle("Consumo Abono");
+			
+			var oModel = new JSONModel( { "Titulo" : "Periodo 09/2018" } );
+			this.getView().byId("panel2").setModel(oModel);
+													
+			
+			
+			
+			var vConsumido = 600;
+			var vRestante = 400;
+			
+			var vPorcentaje =   vConsumido * 100 / ( vConsumido + vRestante );
+			
+			this.getView().byId("textcont").setText("Horas Contratadas: " + vConsumido);
+			this.getView().byId("textcons").setText("Horas Consumidas: " + vRestante);
+			
+			this.getView().byId("radial").setPercentage(vPorcentaje);
+			this.getView().byId("radial").setValueColor("Critical");
+			this.getView().byId("radial").setTooltip("");
+			
+			
+		/*	this.OnBeforeRendering( );*/
+				
+		},
+		
+		OnBeforeRendering: function(){
+			
+	
+			
+			var vModel = sap.ui.getCore().getModel();
+			vModel.getData("/TicketsAbiertosSet");
+			var items = this.byId("/TicketsAbiertosSet").getItems();    
+			
+			for (var item_index = 0; item_index < items.length; item_index++) {
+    			var item = items[item_index];
+			}
 		},
 
 		mapingOData: function (that, oFrame) {
 			
-			var vAbiertos = 38;
-			var vCerrados = 62;
-
+			var vCliente = 38;
+			var vSofttek = 62;
+			var vOtros = 62;
+			
+			var vConsumido = 60;
+			var vRestante = 40;
+			
+			var oModelConsumo = new JSONModel( { 'Consumo' : [ 
+				{
+					nombre: "",
+    				consumido: vConsumido,
+    				restante: vRestante
+				} ] } );
+			
 			var oModel = new JSONModel( { 'Data' : [ 
 				{
-					nombre: "Abiertos",
-    				valor: vAbiertos,
+					nombre: "Cliente",
+    				valor: vCliente,
     				otro: 1
 				},
 				{
-					nombre: "Cerrados",
-    				valor: vCerrados,
+					nombre: "Softtek",
+    				valor: vSofttek,
     				otro: 2
-				}] } );
+				},
+				{
+					nombre: "SPS/PA/SAP",
+    				valor: vOtros,
+    				otro: 3
+				}
+				] } );
 			
 			var oDataSet = new sap.viz.ui5.data.FlattenedDataset({
 				dimensions : [{
-					name: 'nombre',
+					 axis : 1,
+					name: 'Tickets',
 					value: "{nombre}"
 				}],
 
 				measures : [{
-					name: 'valor',
-					value: '{valor}'
-				}
-				,
-				{
-					name: 'otro',
-					value: '{otro}'
-				}
-				],
+					name: 'Consumido',
+					value: '{consumido}'
+				},{
+					name: 'Restante',
+					value: '{restante}'
+				}],
 				data : {
-					path: "/Data"
+					path: "/Consumo"
 				}
-			});
+			
+			} );
 			
 			oFrame.setDataset(oDataSet);
-			oFrame.setModel(oModel);
+			oFrame.setModel(oModelConsumo);
 			this.getView().byId("idDonutChart").setModel(oModel);
 			
 			
-			oFrame.setVizType('stacked_bar');
+			//oFrame.setVizType('stacked_bar');
+			//oFrame.setVizType('vertical_bullet');
+			oFrame.setVizType('bullet');
+			
 			
 
 			oFrame.setVizProperties({
@@ -65,7 +120,7 @@ sap.ui.define([
 					colorPalette: d3.scale.category20().range()
 				},
 				title: {
-					visible: "true",
+					visible: "false",
 					text: "Tickets"
 
 				}
@@ -74,14 +129,15 @@ sap.ui.define([
 			var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
 				'uid': "valueAxis",
 				'type': "Measure",
-				'values': ["valor","otro"]
-			//'values': ["valor"]
+				'values': ["Consumido","Restante"]
+				//'values': ["valor"]
 			});
 
 			var feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
 				'uid': "categoryAxis",
 				'type': "Dimension",
-				'values': ["nombre"]
+				//'values': ["nombre","otro"]
+				'values': ["Tickets"]
 			});
 
 			oFrame.addFeed(feedValueAxis);
@@ -133,6 +189,11 @@ sap.ui.define([
 			oFrame.addFeed(feedValueAxis);
 			oFrame.addFeed(feedCategoryAxis);
 
+		},
+		
+		mensaje:function(){
+			sap.m.MessageBox.show("blabla");
+			
 		}
 	});
 });
